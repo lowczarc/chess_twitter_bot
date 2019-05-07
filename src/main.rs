ab8bdad95c137e0fc1d96b5ea59fd9eb0fc2b895
Lancelot OWCZARCZAK <lowczarc@student.42.fr>
2019-05-07 19:47:25 +0200

    stockfish moves

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,57 @@
-use std::process::{Command, Stdio};
+use std::{
+    io::{BufRead, BufReader, Read, Write},
+    process::{Child, Command, Stdio},
+};
 
-use chess::{Board, Color, File, Game, MoveGen, Rank, Square};
+use chess::{Board, ChessMove, Color, File, Game, MoveGen, Piece, Rank, Square};
 
-fn get_all_moves(game: Game) -> String {
+const move_time: u32 = 500;
+
+fn str_to_chess_move(chess_move: &str) -> ChessMove {
+    let src = Square::from_string(chess_move[..2].to_owned()).expect("not a valid square");
+    let dest = Square::from_string(chess_move[2..4].to_owned()).expect("not a valid square");
+    let promotion: Option<Piece> = match chess_move.chars().nth(4) {
+        Some('q') => Some(Piece::Queen),
+        Some('b') => Some(Piece::Bishop),
+        Some('r') => Some(Piece::Rook),
+        Some('k') => Some(Piece::Knight),
+        _ => None,
+    };
+
+    ChessMove::new(src, dest, promotion)
+}
+
+fn stockfish_move(stockfish: &mut Child, all_moves: String) -> Result<ChessMove, ()> {
+    let mut stdin = stockfish
+        .stdin
+        .as_mut()
+        .expect("Failed to open stockfish stdin");
+
+    let mut stdout = BufReader::new(stockfish.stdout.as_mut().unwrap());
+    let command_sent = &format!(
+        "position startpos moves {}\ngo movetime {}\n",
+        all_moves, move_time
+    );
+
+    let size = stdin
+        .write(command_sent.as_bytes())
+        .expect("Failed to write to stdin");
+
+    for line in stdout.lines() {
+        if let Ok(command) = line {
+            let words: Vec<&str> = command.split(" ").collect();
+            if words[0] == "bestmove" {
+                if words[1] == "(none)" {
+                    return Err(());
+                }
+                return Ok(str_to_chess_move(words[1]));
+            }
+        }
+    }
+    Err(())
+}
+
+fn get_all_moves(game: &Game) -> String {
     return game
         .actions()
         .iter()
@@ -42,12 +91,16 @@ fn main() {
         .spawn()
         .expect("Stockfish failed to launch");
 
-    for _ in 0..5 {
-        game.make_move(MoveGen::new_legal(&game.current_position()).next().unwrap());
+    loop {
+        if game.current_position().status() == chess::BoardStatus::Checkmate {
+            break;
+        }
+        let stockfish_move = stockfish_move(&mut engine, get_all_moves(&game))
+            .expect("Stockfish exited unexpectedly");
+        game.make_move(stockfish_move);
         print_board(game.current_position());
         println!("\n");
     }
-    let moves = get_all_moves(game);
-    println!("{}", moves);
+
     engine.kill().expect("Stockfish failed to shutdown");
 }
